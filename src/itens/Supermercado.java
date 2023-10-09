240a6941f3cd577591ba98257351fa3c141f1093
Gian Bertoldi <65470390+gianbertoldi@users.noreply.github.com>
2023-10-09 19:08:24 -0300

    Correcoes
    Faz a busca agora independente de acento ou esta em maiusculo ou minusculo

--- src/itens/Supermercado.java
+++ src/itens/Supermercado.java
@@ -1,5 +1,6 @@
 package itens;
 
+import java.text.Normalizer;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,12 +18,6 @@ public class Supermercado {
 		adicionarProduto("Bis branco", "Chocolate", 2.70, 50);
 	}
 
-	private void adicionarProduto(String nome, String tipo, double valor, int quantidadeEmEstoque) {
-		int codigo = listaDeProdutos.size(); // Gere o código automaticamente com base na posição na lista
-		Produto produto = new Produto(codigo, nome, tipo, valor, quantidadeEmEstoque);
-		listaDeProdutos.add(produto);
-	}
-
 	public void listarProdutos() {
 		for (Produto produto : listaDeProdutos) {
 			System.out.println(produto);
@@ -30,31 +25,30 @@ public class Supermercado {
 	}
 
 	public void buscarPorNome(String nome) {
+		String nomeSemAcentoInput = removerAcentos(nome).toLowerCase();	
 		boolean encontrou = false;
-
 		for (Produto produto : listaDeProdutos) {
-			if (produto.getNome().toLowerCase().contains(nome.toLowerCase())) {
+			String nomeSemAcentoProduto = removerAcentos(produto.getNome()).toLowerCase();
+			if (nomeSemAcentoProduto.contains(nomeSemAcentoInput)) {
 				System.out.println(produto);
 				encontrou = true;
 			}
 		}
-	
-
 		if (!encontrou) {
 			System.out.println("Nenhum produto encontrado com o nome '" + nome + "'.");
 		}
 	}
 
 	public void buscarPorTipo(String tipo) {
+		String tipoSemAcentoInput = removerAcentos(tipo).toLowerCase();	
 		boolean encontrou = false;
-
 		for (Produto produto : listaDeProdutos) {
-			if (produto.getTipo().toLowerCase().contains(tipo.toLowerCase())) {
+			String tipoSemAcentoProduto = removerAcentos(produto.getTipo()).toLowerCase();
+			if (tipoSemAcentoProduto.contains(tipoSemAcentoInput)) {
 				System.out.println(produto);
 				encontrou = true;
 			}
 		}
-
 		if (!encontrou) {
 			System.out.println("Nenhum produto encontrado com o nome '" + tipo + "'.");
 		}
@@ -65,12 +59,19 @@ public class Supermercado {
 	}
 
 	public void excluirProduto(String nome) {
-		listaDeProdutos.removeIf(produto -> produto.getNome().toLowerCase().contains(nome.toLowerCase()));
+		String nomeSemAcentoInput = removerAcentos(nome).toLowerCase();	
+		listaDeProdutos.removeIf(produto -> {
+			String nomeSemAcentoProduto = removerAcentos(produto.getNome()).toLowerCase();
+        return nomeSemAcentoProduto.contains(nomeSemAcentoInput);
+		});
 	}
 
 	public void alterarValor(String nome, double novoValor) {
 		for (Produto produto : listaDeProdutos) {
-			if (produto.getNome().toLowerCase().contains(nome.toLowerCase())) {
+			String nomeSemAcentoProduto = removerAcentos(produto.getNome()).toLowerCase();
+	        String nomeSemAcentoInput = removerAcentos(nome).toLowerCase();
+			
+			if (nomeSemAcentoProduto.contains(nomeSemAcentoInput)) {
 				produto.setValor(novoValor);
 				break;
 			}
@@ -79,10 +80,29 @@ public class Supermercado {
 
 	public void alterarEstoque(String nome, int novoValor) {
 		for (Produto produto : listaDeProdutos) {
-			if (produto.getNome().toLowerCase().contains(nome.toLowerCase())) {
+			String nomeSemAcentoProduto = removerAcentos(produto.getNome()).toLowerCase();
+	        String nomeSemAcentoInput = removerAcentos(nome).toLowerCase();
+	        
+			if (nomeSemAcentoProduto.contains(nomeSemAcentoInput)) {
 				produto.setQuantidadeEmEstoque(novoValor);
 				break;
 			}
 		}
 	}
+
+	private void adicionarProduto(String nome, String tipo, double valor, int quantidadeEmEstoque) {
+		int codigo = listaDeProdutos.size();
+		Produto produto = new Produto(codigo, nome, tipo, valor, quantidadeEmEstoque);
+		listaDeProdutos.add(produto);
+	}
+	
+	 private String removerAcentos(String texto) {
+	        if (texto == null) {
+	            return null;
+	        }
+	        texto = Normalizer.normalize(texto, Normalizer.Form.NFD)
+	                .replaceAll("\\p{InCombiningDiacriticalMarks}+", "");
+
+	        return texto;
+	    }
 }
