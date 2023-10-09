1c9651ee23924f429b2116c5c1c8a14d7c8fdbd0
Gian Bertoldi <65470390+gianbertoldi@users.noreply.github.com>
2023-10-09 14:14:19 -0300

    Ajuste de linhas 2

--- src/executavel/Main.java
+++ src/executavel/Main.java
@@ -5,9 +5,9 @@ import itens.Menu;
 public class Main {
 
 	static Menu menu = new Menu();
-	
+
 	public static void main(String[] args) {
-        menu.opcoesMenu();
-        menu.selecaoMenu();
-    }
+		menu.opcoesMenu();
+		menu.selecaoMenu();
+	}
 }
--- src/itens/Menu.java
+++ src/itens/Menu.java
@@ -10,41 +10,34 @@ public class Menu {
 	String nome;
 	String tipo;
 	int quantidade;
-	
-	public void opcoesMenu() {		
-		System.out.println("Menu\n"
-				+ "1)Listar produtos\r\n"
-				+ "2)Busca de produtos (nome)\r\n"
-				+ "3)Busca de produtos (tipo)\r\n"
-				+ "4)Cadastrar produtos\r\n"
-				+ "5)Excluir produto\r\n"
-				+ "6)Alterar valor de produto\r\n"
-				+ "7)Alterar estoque de produto\r\n"
-				+ "0) Encerrar");
+
+	public void opcoesMenu() {
+		System.out.println("Menu\n" + "1)Listar produtos\r\n" + "2)Busca de produtos (nome)\r\n"
+				+ "3)Busca de produtos (tipo)\r\n" + "4)Cadastrar produtos\r\n" + "5)Excluir produto\r\n"
+				+ "6)Alterar valor de produto\r\n" + "7)Alterar estoque de produto\r\n" + "0) Encerrar");
 	}
-	
-	
-	public void selecaoMenu() { 
-		int opcMenu;		
-		 opcMenu = key.nextInt();		 
-		 while (opcMenu != 0) {
-			 switch (opcMenu) {
-			case 1: 
+
+	public void selecaoMenu() {
+		int opcMenu;
+		opcMenu = key.nextInt();
+		while (opcMenu != 0) {
+			switch (opcMenu) {
+			case 1:
 				comandos.listarProdutos();
 				opcoesMenu();
-				opcMenu = key.nextInt();	
+				opcMenu = key.nextInt();
 				break;
 			case 2:
 				System.out.println("Qual o Produto que deseja:");
 				comandos.buscarPorNome(key.next());
 				opcoesMenu();
-				opcMenu = key.nextInt();	
+				opcMenu = key.nextInt();
 				break;
 			case 3:
 				System.out.println("Qual o tipo de produto que deseja:");
 				comandos.buscarPorTipo(key.next());
 				opcoesMenu();
-				opcMenu = key.nextInt();	
+				opcMenu = key.nextInt();
 				break;
 			case 4:
 				System.out.println("Cadastro de produto:");
@@ -58,14 +51,14 @@ public class Menu {
 				quantidade = key.nextInt();
 				comandos.cadastrarProduto(nome, tipo, valor, quantidade);
 				opcoesMenu();
-				opcMenu = key.nextInt();	
+				opcMenu = key.nextInt();
 				break;
-				
+
 			case 5:
 				System.out.println("Qual produto ira excluir:");
 				comandos.excluirProduto(key.next());
 				opcoesMenu();
-				opcMenu = key.nextInt();	
+				opcMenu = key.nextInt();
 				break;
 			case 6:
 				System.out.println("qual o produto que ira trocar o valor");
@@ -74,7 +67,7 @@ public class Menu {
 				valor = key.nextDouble();
 				comandos.alterarValor(nome, valor);
 				opcoesMenu();
-				opcMenu = key.nextInt();	
+				opcMenu = key.nextInt();
 				break;
 			case 7:
 				System.out.println("qual o produto que ira mudar  quantidade no estoque");
@@ -83,14 +76,14 @@ public class Menu {
 				valor = key.nextDouble();
 				comandos.alterarEstoque(nome, quantidade);
 				opcoesMenu();
-				opcMenu = key.nextInt();	
+				opcMenu = key.nextInt();
 				break;
 			default:
 				System.out.println("Comando ivalido");
 				opcoesMenu();
-				opcMenu = key.nextInt();	
+				opcMenu = key.nextInt();
 				break;
 			}
-		 }
+		}
 	}
 }
